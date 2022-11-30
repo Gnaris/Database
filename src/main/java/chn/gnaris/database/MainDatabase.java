1bc1df9914ac3da661f41c1173501b1e9f378d1a
Gnaris <devcodedred@gmail.com>
2022-11-30 01:18:42 +0100

    Fin
    Signed-off-by: Gnaris <Gnaris88@hotmail.com>

--- src/main/java/chn/gnaris/database/MainDatabase.java
+++ src/main/java/chn/gnaris/database/MainDatabase.java
@@ -6,7 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class Main extends JavaPlugin {
+public class MainDatabase extends JavaPlugin {
 
     private Database database;
 
